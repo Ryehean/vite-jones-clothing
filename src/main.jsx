52f9869d2ac53f65060572c9d60463911f09f962
Ryehean Jones <ryehean@gmail.com>
2023-01-03 16:25:07 -0500

    converted entire app to use styled components.

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import {
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
-import { ProductsProvider } from './contexts/products.context';
+import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from "./contexts/cart.context";
 
 import "./index.scss";
@@ -16,11 +16,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
 						<App />
 					</CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
